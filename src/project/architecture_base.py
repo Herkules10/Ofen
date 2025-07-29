e6b68ccf31e30c0b31d7bd458452d3003afe278b
Joshua Durrant <jdurrant@student.ethz.ch>
2025-07-29 16:58:27 +0200

    GA working and folder structure changes

--- src/project/architecture_base.py
+++ src/project/architecture_base.py
@@ -35,6 +35,11 @@ class NetworkArchitecture:
         self.num_classes = num_classes
         self.layers: List[LayerConfig] = []
         self._current_shape = input_shape
+    
+    @property
+    def current_shape(self):
+        """Get the current shape after all layers."""
+        return self._current_shape
         
     def add_layer(self, layer_config: LayerConfig) -> bool:
         """Add a layer to the architecture. Returns True if successful."""
@@ -76,30 +81,60 @@ class NetworkArchitecture:
         return False
     
     def _update_current_shape(self, layer_config: LayerConfig):
-        """Update current tensor shape after adding layer."""
-        if layer_config.layer_type == LayerType.CONV2D:
-            c_in, h_in, w_in = self._current_shape
-            filters = layer_config.params['filters']
-            kernel_size = layer_config.params['kernel_size']
-            stride = layer_config.params.get('stride', 1)
-            padding = layer_config.params.get('padding', 0)
+        """Update current shape after adding a layer."""
+        if len(self._current_shape) == 3:  # (C, H, W)
+            c, h, w = self._current_shape
             
-            h_out = (h_in + 2*padding - kernel_size) // stride + 1
-            w_out = (w_in + 2*padding - kernel_size) // stride + 1
-            self._current_shape = (filters, h_out, w_out)
+            if layer_config.layer_type == LayerType.CONV2D:
+                # Ensure stride has a default value
+                stride = layer_config.params.get('stride', 1)
+                if stride is None:
+                    stride = 1
+                
+                padding = layer_config.params.get('padding', 0)
+                if padding is None:
+                    padding = 0
+                
+                kernel_size = layer_config.params.get('kernel_size', 3)
+                if kernel_size is None:
+                    kernel_size = 3
+                
+                filters = layer_config.params.get('filters', 32)
+                if filters is None:
+                    filters = 32
+                
+                # Calculate output dimensions
+                h_out = (h + 2 * padding - kernel_size) // stride + 1
+                w_out = (w + 2 * padding - kernel_size) // stride + 1
+                self._current_shape = (filters, h_out, w_out)
             
-        elif layer_config.layer_type == LayerType.MAXPOOL:
-            if len(self._current_shape) == 3:  # Conv2D
-                c, h, w = self._current_shape
+            elif layer_config.layer_type == LayerType.MAXPOOL:
                 pool_size = layer_config.params.get('pool_size', 2)
-                stride = layer_config.params.get('stride', pool_size)
+                if pool_size is None:
+                    pool_size = 2
+                
+                stride = layer_config.params.get('stride', pool_size)  # Default stride = pool_size
+                if stride is None:
+                    stride = pool_size
+                
                 h_out = h // stride
                 w_out = w // stride
                 self._current_shape = (c, h_out, w_out)
-                
-        elif layer_config.layer_type == LayerType.FC:
-            units = layer_config.params['units']
-            self._current_shape = (units,)
+            
+            elif layer_config.layer_type in [LayerType.DROPOUT, LayerType.BATCHNORM]:
+                # Shape doesn't change
+                pass
+        
+        elif len(self._current_shape) == 1:  # Already flattened
+            if layer_config.layer_type == LayerType.FC:
+                units = layer_config.params.get('units', 128)
+                if units is None:
+                    units = 128
+                self._current_shape = (units,)
+            
+            elif layer_config.layer_type in [LayerType.DROPOUT, LayerType.BATCHNORM]:
+                # Shape doesn't change
+                pass
     
     def _recalculate_shapes(self):
         """Recalculate all shapes after layer removal."""
@@ -285,9 +320,35 @@ class ArchitectureGenerator:
     def generate_random_architecture(self) -> NetworkArchitecture:
         """Generate a random valid architecture."""
         arch = NetworkArchitecture(self.input_shape, self.num_classes)
-        num_layers = random.randint(1, self.max_layers)
+        num_layers = random.randint(2, self.max_layers)
         
-        for _ in range(num_layers):
+        # For image data, start with some conv layers
+        if len(self.input_shape) == 3:
+            # Add 1-3 conv/pool layers
+            conv_layers = random.randint(1, min(3, num_layers // 2))
+            for _ in range(conv_layers):
+                layer_config = self._generate_random_layer(arch)
+                if layer_config and arch.add_layer(layer_config):
+                    continue
+                else:
+                    break
+            
+            # Force transition to FC layers if still in conv space
+            if len(arch.current_shape) == 3:
+                # Add a FC layer to transition from conv to fully connected
+                flat_size = arch.current_shape[0] * arch.current_shape[1] * arch.current_shape[2]
+                # Don't add if the flattened size would be too large
+                if flat_size <= 10000:  # Reasonable limit
+                    fc_layer = LayerConfig(
+                        layer_type=LayerType.FC,
+                        params={'units': random.choice([128, 256, 512])}
+                    )
+                    arch.layers.append(fc_layer)
+                    arch._current_shape = (fc_layer.params['units'],)
+        
+        # Add remaining layers (FC, dropout, etc.)
+        remaining_layers = num_layers - len(arch.layers)
+        for _ in range(max(0, remaining_layers)):
             layer_config = self._generate_random_layer(arch)
             if layer_config and arch.add_layer(layer_config):
                 continue
@@ -296,11 +357,10 @@ class ArchitectureGenerator:
         
         # Ensure we have at least one meaningful layer
         if len(arch.layers) == 0:
-            # Add a simple conv layer
             if len(self.input_shape) == 3:  # Image data
                 layer_config = LayerConfig(
                     LayerType.CONV2D,
-                    {'filters': 32, 'kernel_size': 3, 'padding': 1}
+                    {'filters': 32, 'kernel_size': 3, 'stride': 1, 'padding': 1}
                 )
             else:
                 layer_config = LayerConfig(
@@ -311,71 +371,62 @@ class ArchitectureGenerator:
         
         return arch
     
-    def _generate_random_layer(self, arch: NetworkArchitecture) -> Optional[LayerConfig]:
-        """Generate a random layer configuration."""
-        current_shape = arch._current_shape
+    def _generate_random_layer(self, current_arch: NetworkArchitecture) -> LayerConfig:
+        """Generate a random layer compatible with current architecture."""
+        current_shape = current_arch.current_shape
         
-        # Choose layer type based on current architecture state
-        possible_layers = []
+        if len(current_shape) == 3:  # Can add conv, pool, dropout, batchnorm
+            # Weight the choices - favor conv and pool layers
+            layer_types = [LayerType.CONV2D] * 3 + [LayerType.MAXPOOL] * 2 + [LayerType.DROPOUT, LayerType.BATCHNORM]
+            layer_type = random.choice(layer_types)
+            
+            if layer_type == LayerType.CONV2D:
+                return LayerConfig(
+                    layer_type=LayerType.CONV2D,
+                    params={
+                        'filters': random.choice([16, 32, 64, 128, 256]),
+                        'kernel_size': random.choice([3, 5]),
+                        'stride': random.choice([1, 2]),
+                        'padding': random.choice([0, 1]),
+                        'activation': 'relu'
+                    }
+                )
+            elif layer_type == LayerType.MAXPOOL:
+                pool_size = random.choice([2, 3])
+                return LayerConfig(
+                    layer_type=LayerType.MAXPOOL,
+                    params={
+                        'pool_size': pool_size,
+                        'stride': pool_size,  # Default stride = pool_size
+                        'padding': 0
+                    }
+                )
+            elif layer_type == LayerType.DROPOUT:
+                return LayerConfig(
+                    layer_type=LayerType.DROPOUT,
+                    params={'rate': random.uniform(0.1, 0.5)}
+                )
+            elif layer_type == LayerType.BATCHNORM:
+                return LayerConfig(layer_type=LayerType.BATCHNORM, params={})
         
-        if len(current_shape) == 3:  # Can add Conv2D, MaxPool, BatchNorm, Dropout, FC
-            possible_layers.extend([LayerType.CONV2D, LayerType.MAXPOOL, LayerType.FC])
-            if len(arch.layers) > 0:
-                possible_layers.extend([LayerType.BATCHNORM, LayerType.DROPOUT, LayerType.ACTIVATION])
-        
-        elif len(current_shape) == 2:  # Can add Conv1D, MaxPool, BatchNorm, Dropout, FC
-            possible_layers.extend([LayerType.CONV1D, LayerType.MAXPOOL, LayerType.FC])
-            if len(arch.layers) > 0:
-                possible_layers.extend([LayerType.BATCHNORM, LayerType.DROPOUT, LayerType.ACTIVATION])
-        
-        else:  # Already flattened, can add FC, BatchNorm, Dropout
-            possible_layers.append(LayerType.FC)
-            if len(arch.layers) > 0:
-                possible_layers.extend([LayerType.BATCHNORM, LayerType.DROPOUT, LayerType.ACTIVATION])
-        
-        if not possible_layers:
-            return None
-        
-        layer_type = random.choice(possible_layers)
-        
-        if layer_type == LayerType.CONV2D:
-            return LayerConfig(LayerType.CONV2D, {
-                'filters': random.choice([16, 32, 64, 128, 256]),
-                'kernel_size': random.choice([3, 5, 7]),
-                'stride': random.choice([1, 2]),
-                'padding': random.choice([0, 1, 2])
-            })
-        
-        elif layer_type == LayerType.CONV1D:
-            return LayerConfig(LayerType.CONV1D, {
-                'filters': random.choice([16, 32, 64, 128]),
-                'kernel_size': random.choice([3, 5, 7]),
-                'stride': random.choice([1, 2]),
-                'padding': random.choice([0, 1, 2])
-            })
-        
-        elif layer_type == LayerType.FC:
-            return LayerConfig(LayerType.FC, {
-                'units': random.choice([64, 128, 256, 512, 1024])
-            })
-        
-        elif layer_type == LayerType.BATCHNORM:
-            return LayerConfig(LayerType.BATCHNORM, {})
-        
-        elif layer_type == LayerType.MAXPOOL:
-            return LayerConfig(LayerType.MAXPOOL, {
-                'pool_size': random.choice([2, 3, 4]),
-                'stride': None  # Will use pool_size as stride
-            })
-        
-        elif layer_type == LayerType.DROPOUT:
-            return LayerConfig(LayerType.DROPOUT, {
-                'rate': random.uniform(0.1, 0.5)
-            })
-        
-        elif layer_type == LayerType.ACTIVATION:
-            return LayerConfig(LayerType.ACTIVATION, {
-                'type': random.choice(list(ActivationType))
-            })
+        elif len(current_shape) == 1:  # Can add FC, dropout, batchnorm
+            layer_types = [LayerType.FC] * 2 + [LayerType.DROPOUT, LayerType.BATCHNORM]
+            layer_type = random.choice(layer_types)
+            
+            if layer_type == LayerType.FC:
+                return LayerConfig(
+                    layer_type=LayerType.FC,
+                    params={
+                        'units': random.choice([64, 128, 256, 512]),
+                        'activation': 'relu'
+                    }
+                )
+            elif layer_type == LayerType.DROPOUT:
+                return LayerConfig(
+                    layer_type=LayerType.DROPOUT,
+                    params={'rate': random.uniform(0.1, 0.5)}
+                )
+            elif layer_type == LayerType.BATCHNORM:
+                return LayerConfig(layer_type=LayerType.BATCHNORM, params={})
         
         return None
